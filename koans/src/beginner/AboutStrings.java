e90cb45d6de6c42554dfb3ffca62ada9a7a4e736
ericweidman <ericweidman@gmail.com>
2016-02-18 21:22:57 -0500

    Finished first 50 problems.

--- koans/src/beginner/AboutStrings.java
+++ koans/src/beginner/AboutStrings.java
@@ -12,7 +12,7 @@ public class AboutStrings {
 
 	@Koan
 	public void implicitStrings() {
-		assertEquals("just a plain ole string".getClass(), __);
+		assertEquals("just a plain ole string".getClass(), String.class);
 	}
 
 	@Koan
@@ -21,14 +21,14 @@ public class AboutStrings {
 		// practice - generally it is redundant, and done repetitively can be slow
 		String string = new String();
 		String empty = "";
-		assertEquals(string.equals(empty), __);
+		assertEquals(string.equals(empty), true);
 	}
 
 	@Koan
 	public void newStringIsRedundant() {
 		String stringInstance = "zero";
 		String stringReference = new String(stringInstance);
-		assertEquals(stringInstance.equals(stringReference), __);
+		assertEquals(stringInstance.equals(stringReference), true);
 	}
 
 	@Koan
